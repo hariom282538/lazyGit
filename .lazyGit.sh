ef0863ebb38d58e339c26c2d26e3fa59dd134778
Hariom Vashisth <hariom.vashisth@airtel.com>
2021-01-03 17:59:18 +0530

    -d

--- .lazyGit.sh
+++ .lazyGit.sh
@@ -1,96 +1,134 @@
 #!/bin/bash
 
-function lazygit() {
-    if [ -d .git ]; then
-        if [[ $(git config user.name) ]]; then
-            echo "Global Git configurations found!"
-            # show user details
-            echo "Configured Git Name: " $(git config user.name)
+pull() {
+
+    if [ $(git status --porcelain=v1 2>/dev/null | wc -l) ] >=1; then
+        echo "Untracked changes: $(git update-index --refresh)"
+        echo "commit or stash changes"
+    else
+        echo "go and pull changeas"
+    fi
+    echo "select option for publishing new local commits on a remote server"
+    echo "1. Pull Current Branch $(git rev-parse --abbrev-ref HEAD)"
+    echo "2. Pull from Existing Other Branch"
+    echo "3. Pull specific commits [cherry-pick commits]"
+    read -p "Please select an option " pullOption
+}
+
+push() {
+    git add .
+    git commit --allow-empty -a -m "$*"
+    # give user to select/create branch
+    echo "select option for publishing new local commits on a remote server"
+    echo "1. Current Branch $(git rev-parse --abbrev-ref HEAD)"
+    echo "2. Existing Branch"
+    echo "3. New Branch"
+    read -p "Please select an option " branchOption
+
+    case $branchOption in
+    1 | c | C)
+        git push -u origin $(git rev-parse --abbrev-ref HEAD)
+        return
+        ;;
+    2 | e | E)
+        echo "Loading all local and remote branches..."
+        selectedBranch=$(git branch -a | tr -s '*' ' ' | tr -d "[:blank:]" | cut -d/ -f3 | sort -u | select_from_list)
+        local STATUS=$?
+        # Check if user selected something
+        if [ $STATUS == 0 ]; then
+            echo "Branch selected by user:" $selectedBranch
+            if [[ $selectedBranch=$(git rev-parse --abbrev-ref HEAD) ]]; then
+                git push -u origin $(git rev-parse --abbrev-ref HEAD)
+            else
+                commitedCodeBranch=$(git rev-parse --abbrev-ref HEAD)
+                git checkout $selectedBranch
+                git merge $commitedCodeBranch
+                git push -U origin $selectedBranch
+            fi
         else
-            echo "No global git configuartion found on this system"
-            # steps for adding user details
-            git config --global credential.helper store
-            read -p "Enter your git name:  " gitName
-            git config --global --add user.name $gitName
-            read -p "Enter your git email: " gitEmail
-            git config --global --add user.email $gitEmail
+            echo "Cancelled!"
         fi
+
+        return
+        ;;
+    3 | n | N)
+        read -p "Enter branch name: " newBranch
+        git checkout -b $newBranch
+        git push -u origin $newBranch
+        return
+        ;;
+    *) echo "Please answer 1, 2 or 3." ;;
+    esac
+
+}
+
+cherrypick() {
+    echo "cherrypick"
+
+}
+
+show() {
+    echo "show"
+}
+
+init() {
+    read -p "Initialize the local directory as a Git repository? [Y/n]" gitSetup
+    case $gitSetup in
+    [Yy]*)
+        git init
+        if [[ $(git config user.name) ]]; then
+            echo "Git configurations found!"
+            echo "Configured Git Name: $(git config user.name)"
+            echo "Configured Git Email: $(git config user.email)"
+            read -p "Wanted to change? [Y/n]" gitConfigChange
+            case $gitConfigChange in
+            [Yy]*)
+                config
+                return
+                ;;
+            [Nn]*)
+                return
+                ;;
+            esac
+
+        else
+            echo "No git configuartion on this system"
+            config
+        fi
+
+        read -p "Push to existing Git Repositary. Repo URL? [ex: git@bitbucket.org:USER/REPO.git]" gitRepo
+        git remote add origin $gitRepo
+        git remote -v
         git add .
         git commit --allow-empty -a -m "$*"
-        # give user to select/create branch
-        echo "select option for publishing new local commits on a remote server"
-        echo "1. Current Branch $(git rev-parse --abbrev-ref HEAD)"
-        echo "2. Existing Branch"
-        echo "3. New Branch"
-        read -p "Please select an option " branchOption
-        echo $branchOption
-        case $branchOption in
-        1 | c | C)
-            git push -u origin $(git rev-parse --abbrev-ref HEAD)
-            return
-            ;;
-        2 | e | E)
-            echo "Loading all local and remote branches..."
-            selectedBranch=$(git branch -a | tr -s '*' ' ' | tr -d "[:blank:]" | cut -d/ -f3 | sort -u | select_from_list)
-            local STATUS=$?
-            # Check if user selected something
-            if [ $STATUS == 0 ]; then
-                echo "Branch selected by user:" $selectedBranch
-                if [[ $selectedBranch=$(git rev-parse --abbrev-ref HEAD) ]]; then
-                    git push -u origin $(git rev-parse --abbrev-ref HEAD)
-                else
-                    commitedCodeBranch=$(git rev-parse --abbrev-ref HEAD)
-                    git checkout $selectedBranch
-                    git merge $commitedCodeBranch
-                    git push origin $selectedBranch
-                fi
-            else
-                echo "Cancelled!"
-            fi
+        git push -u origin master
+        return
+        ;;
+    [Nn]*) return ;;
+    *) echo "Please answer yes or no." ;;
+    esac
+}
 
-            return
-            ;;
-        3 | n | N)
-            read -p "Enter branch name: " newBranch
-            git checkout -b $newBranch
-            git push -u origin $newBranch
-            return
-            ;;
-        *) echo "Please answer 1, 2 or 3." ;;
-        esac
-
-    else
-
-        read -p "Initialize the local directory as a Git repository? [Y/n]" gitSetup
-        case $gitSetup in
-        [Yy]*)
-            git init
-            if [[ $(git config user.name) ]]; then
-                echo "Global Git configurations found!"
-                # show them user details
-                echo "Configured Git Name: " $(git config user.name)
-            else
-                echo "No gobal git configuartion on this system"
-                # steps for adding user details
-                git config --global credential.helper store
-                read -p "Enter your git name:  " gitName
-                git config --global --add user.name $gitName
-                read -p "Enter your git email: " gitEmail
-                git config --global --add user.email $gitEmail
-            fi
-
-            read -p "Push to existing Git Repositary. Repo URL? [ex: git@bitbucket.org:USER/REPO.git]" gitRepo
-            git remote add origin $gitRepo
-            git remote -v
-            git add .
-            git commit --allow-empty -a -m "$*"
-            git push -u origin master
-            return
-            ;;
-        [Nn]*) return ;;
-        *) echo "Please answer yes or no." ;;
-        esac
-    fi
+config() {
+    read -p "Global(system level) or Local(project level)? [G/l]" gitConfigSetup
+    case $gitConfigSetup in
+    [Gg]*)
+        git config --global credential.helper store
+        read -p "Enter your git name:  " gitName
+        git config --global --add user.name $gitName
+        read -p "Enter your git email: " gitEmail
+        git config --global --add user.email $gitEmail
+        return
+        ;;
+    [Ll]*)
+        git config --local credential.helper store
+        read -p "Enter your git name:  " gitName
+        git config --local --add user.name $gitName
+        read -p "Enter your git email: " gitEmail
+        git config --local --add user.email $gitEmail
+        return
+        ;;
+    esac
 }
 
 select_from_list() {
@@ -130,3 +168,44 @@ select_from_list() {
     done
     echo $opt
 }
+
+function lazygit() {
+    options_found=0
+    while getopts ":uds" opt; do
+        options_found=1
+        case $opt in
+        d)
+            echo "pull was triggered, Parameter: $OPTARG" >&2
+            pull
+            ;;
+        u)
+            echo "push was triggered, Parameter: $OPTARG" >&2
+            push
+            ;;
+        s)
+            echo "show was triggered, Parameter: $OPTARG" >&2
+            show
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+        esac
+    done
+
+    if ((!options_found)); then
+        echo "no options found"
+    fi
+
+}
+
+unset -f pull
+unset -f push
+unset -f gitConfigChangeinit
+unset -f show
+unset -f cherrypick
+unset -f select_from_list
