501360b2d680241044633eeec1ef97f78d397891
hariom282538 <hariom.devops@gmail.com>
2022-06-02 19:27:24 +0530

    c0

--- .lazyGit.sh
+++ .lazyGit.sh
@@ -72,7 +72,7 @@ push() {
     echo "1. Current Branch $(git rev-parse --abbrev-ref HEAD)"
     echo "2. Existing Branch"
     echo "3. New Branch"
-    read -p "Please select an option " branchOption
+    echo "Please select an option " read branchOption
 
     case $branchOption in
     1 | c | C)
